fd33c091ba3bbd32aec56879a00ced33707540c9
wata57 <felipe.watai@gmail.com>
2024-01-07 22:24:58 -0300

    Update day5part2.py

--- AOC/day5/day5part2.py
+++ AOC/day5/day5part2.py
@@ -2,7 +2,7 @@ def conversion(numbers, maps):
     result = []
     for n in numbers:
         for line in maps:
-            s,d,r = line.split(' ')
+            d,s,r = line.split(' ')
             if int(s) <= int(n) <= int(s) + int(r) - 1:
                     i = int(n) - int(s) + int(d)
                     result.append(i)
@@ -11,53 +11,38 @@ def conversion(numbers, maps):
             result.append(int(n))
     return result
 
+def maps(n):
+    maP = s[n].split('\n')
+    maP.pop(0)
+    return maP
+
 with open('input.txt', 'r') as file:
     data = file.read()
     s = data.strip().split('\n\n')
 
+# SEEDS
+title, seeds_numbers = s[0].split(': ')
+seeds = seeds_numbers.split(' ')
 
-# MAPS
 # SEED TO SOIL
-m1 = s[1].split('\n')
-m1.pop(0)
+soil_numbers = conversion(seeds, maps(1))
+
 # SOIL TO FERTILIZER
-m2 = s[2].split('\n')
-m2.pop(0)
+fertilizer_numbers = conversion(soil_numbers, maps(2))     
+
 # FERTILIZER TO WATER
-m3 = s[3].split('\n')
-m3.pop(0)
+water_numbers = conversion(fertilizer_numbers, maps(3))
+
 # WATER TO LIGHT
-m4 = s[4].split('\n')
-m4.pop(0)
+light_numbers = conversion(water_numbers, maps(4))
+
 # LIGHT TO TEMPERATURE
-m5 = s[5].split('\n')
-m5.pop(0)
+temperature_numbers = conversion(light_numbers, maps(5))
+
 # TEMPERATURE TO HUMIDITY
-m6 = s[6].split('\n')
-m6.pop(0)
-#HUMIDITY TO LOCATION
-m7 = s[7].split('\n')
-m7.pop(0)
+humidity_numbers = conversion(temperature_numbers, maps(6))
 
-# LOCATION TO HUMIDITY
-humidity_numbers = conversion(location_numbers, m7)
-
-# HUMIDITY TO TEMPERATURE
-temperature_numbers = conversion(humidity_numbers, m6)     
-
-# TEMPERATURE TO LIGHT
-light_numbers = conversion(temperature_numbers, m5)
-
-# LIGHT TO WATER
-water_numbers = conversion(light_numbers, m4)
-
-# WATER TO FERTILIZER
-fertilizer_numbers = conversion(water_numbers, m3)
-
-# FERTILIZER TO SOIL
-soil_numbers = conversion(fertilizer_numbers, m2)
-
-# SOIL TO SEED
-location_numbers = conversion(humidity_numbers, m7)
+# HUMIDITY TO LOCATION
+location_numbers = conversion(humidity_numbers, maps(7))
 
 print(min(location_numbers))
