87353106e13daa0fb0dd6650301ba395f8efcfad
wata57 <felipe.watai@gmail.com>
2024-01-08 20:18:31 -0300

    Update day5part2.py

--- AOC/day5/day5part2.py
+++ AOC/day5/day5part2.py
@@ -1,28 +1,50 @@
 def conversion(numbers, maps):
     result = []
-    for n in numbers:
+    while len(numbers) > 0:
+        a, b = numbers.pop()
+        temp = []
         for line in maps:
-            d,s,r = line.split(' ')
-            if int(s) <= int(n) <= int(s) + int(r) - 1:
-                    i = int(n) - int(s) + int(d)
-                    result.append(i)
-                    break
-        else:
-            result.append(int(n))
+            d, s, r = map(int, line.split(' '))
+            if s <= a < s + r and b <= s + r:
+                temp.append((a - s + d, b - s + d))
+                break
+            elif s > a and b > s + r:
+                numbers.append((a, s - 1))
+                temp.append((s - s + d, s + r - s + d))
+                numbers.append((r + s + 1, b))
+                break
+            elif s > a and s <= b <= s + r:
+                numbers.append((a, s - 1))
+                temp.append((s - s + d, b - s + d))
+                break
+            elif s <= a < s + r and b > s + r:
+                temp.append((a - s + d, s + r - s + d))
+                numbers.append((s + r, b))
+                break
+        if len(temp) == 0:
+            temp.append((a, b))
+        result += temp
     return result
 
-def maps(n):
-    maP = s[n].split('\n')
+def maps(x):
+    maP = parsed_data[x].split('\n')
     maP.pop(0)
     return maP
 
 with open('input.txt', 'r') as file:
     data = file.read()
-    s = data.strip().split('\n\n')
+    parsed_data = data.strip().split('\n\n')
 
 # SEEDS
-title, seeds_numbers = s[0].split(': ')
-seeds = seeds_numbers.split(' ')
+title, seeds_numbers = parsed_data[0].split(': ')
+seeds_range = seeds_numbers.split(' ')
+
+i = 0
+seeds = []
+while i < len(seeds_range):
+    x, y = map(int, (seeds_range[i], seeds_range[i+1]))
+    seeds.append((x, x + y))
+    i += 2
 
 # SEED TO SOIL
 soil_numbers = conversion(seeds, maps(1))
@@ -45,4 +67,4 @@ humidity_numbers = conversion(temperature_numbers, maps(6))
 # HUMIDITY TO LOCATION
 location_numbers = conversion(humidity_numbers, maps(7))
 
-print(min(location_numbers))
+print(min(location_numbers)[0])
